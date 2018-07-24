f9c438e016748b81a452ffe09e00c4a9b91dca10
josediaz16 <jldiaz1612@gmail.com>
2018-07-24 14:25:29 -0500

    temporal

--- hound.exs
+++ hound.exs
@@ -2,21 +2,34 @@ defmodule HoundExample do
 
   use Hound.Helpers
 
-  def start do
-    Hound.start_session
-    navigate_to "http://www.instagram.com/elephantjournal"
-    execute_script "window.document.body.scrollTop = document.body.scrollHeight;"
-
-    page_source()
-    |> Floki.find(".v1Nh3.kIKUG._bz0w a")
-    |> Floki.attribute("href")
-    |> Enum.map(fn code -> "https://www.instagram.com#{code}" end)
+  def start(screen_name) do
+    navigate_to "http://www.instagram.com/#{screen_name}"
+    (1 .. 16)
+      |> Enum.flat_map(fn _ ->
+        execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        Process.sleep(1000)
+        page_source()
+          |> Floki.find(".v1Nh3.kIKUG._bz0w a")
+          |> Floki.attribute("href")
+          |> Enum.map(fn code -> "https://www.instagram.com#{code}" end)
+    end)
+      |> Enum.uniq
   end
 end
 
-links = HoundExample.start
-IO.puts inspect(links)
-results = Collector.Example.from_links(links)
+Hound.start_session
+screen_names = File.stream!("new_names_3") |> Stream.map(&String.trim_trailing/1)  |> Enum.to_list |> Enum.slice(491, 8)
 
-IO.puts length(results)
-Enum.each(results, fn result -> IO.puts(inspect(result)) end)
+screen_names
+  |> Enum.map(fn screen_name -> 
+    IO.puts screen_name
+    links = HoundExample.start(screen_name)
+    [%{screen_name => links, "count" => length(links)}]
+      |> Stream.map(&(inspect(&1, limit: :infinity) <> "\n"))
+      |> Stream.into(File.stream!("los_screen_names_4", [:append, :utf8]))
+      |> Stream.run
+
+  end)
+
+Hound.end_session
+
